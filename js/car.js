d3618857ee89313395fc30cb27f031eeab33d921
changfengE <3055784505@qq.com>
2023-07-03 11:49:27 +0800

    cart module

--- js/car.js
+++ js/car.js
@@ -3,7 +3,7 @@
  * @Email: 3517134128@qq.com
  * @Date: 2023-05-26 00:00:39 
  * @Last Modified by: 3517134128@qq.com
- * @Last Modified time: 2023-05-26 00:54:06
+ * @Last Modified time: 2023-07-03 11:41:03
  * @Description: 购物车模块（jQuery）
  */
 $(function() {
@@ -32,7 +32,11 @@ $(function() {
         // 4.计算商品小计，根据文本框的值乘以当前商品的单价即商品小计
         var p = $(this).parent().parent().siblings('.p-price').html();
         p = p.substr(1);
-        $(this).parent().parent().siblings('.p-sum').html('￥' + p * n);
+        // 保留两位小数
+        var price = (p * n).toFixed(2);
+        // parents()返回所有父级元素
+        $(this).parents('.p-num').siblings('.p-sum').html('￥' + price);
+        getSum();
     });
     $('.decrement').click(function() {
         var n = $(this).siblings('.itxt').val();
@@ -42,8 +46,37 @@ $(function() {
         n--;
         $(this).siblings('.itxt').val(n);
 
-        var p = $(this).parent().parent().siblings('.p-price').html();
+        // var p = $(this).parent().parent().siblings('.p-price').html();
+        var p = $(this).parents('.p-num').siblings('.p-price').html();
         p = p.substr(1);
-        $(this).parent().parent().siblings('.p-sum').html('￥' + p * n);
+        $(this).parents('.p-num').siblings('.p-sum').html('￥' + (p * n).toFixed(2));
+        getSum();
     });
+    // 4.用户修改文本框里的值，计算小计模块
+    $('.itxt').change(function() {
+        // 先得到文本框的值*当前商品单价
+        var n = $(this).val();
+        // 单价
+        var p = $(this).parents('.p-num').siblings('.p-price').html();
+        p = p.substr(1);
+        $(this).parents('.p-num').siblings('.p-sum').html('￥' + (p * n).toFixed(2));
+        getSum();
+    });
+
+    // 5.计算总计和总额模块
+    // 页面打开先调用一次
+    getSum();
+
+    function getSum() {
+        var count = 0;
+        var money = 0;
+        $('.itxt').each(function(i, ele) {
+            count += parseInt($(ele).val());
+        });
+        $('.amount-sum em').text(count);
+        $('.p-sum').each(function(i, ele) {
+            money += parseFloat($(ele).text().substr(1))
+        });
+        $('.price-sum em').text('￥' + money.toFixed(2));
+    }
 })
